9ce1813c78877d813818d2b6145e6582629be171
hashim715 <hashimmuhammad844@gmail.com>
2024-08-23 10:48:55 +0530

    added new redis client that is running on the server

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -7,7 +7,7 @@ export const pubClient: Redis = new Redis(process.env.REDIS_URI);
 
 export const subClient: Redis = new Redis(process.env.REDIS_URI);
 
-export const redisClient: Redis = new Redis(process.env.REDIS_URI);
+export const redisClient: Redis = new Redis(process.env.REDISCLIENTURI);
 
 pubClient.on("connect", () => {
   console.log("pubClient connected");
