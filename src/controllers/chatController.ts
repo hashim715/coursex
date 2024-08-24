5281ad2776160b46735af33020fc8b2cc837f350
hashim715 <hashimmuhammad844@gmail.com>
2024-08-24 17:18:58 +0530

    added cloudinary for uploading images

--- src/controllers/chatController.ts
+++ src/controllers/chatController.ts
@@ -252,8 +252,8 @@ export const uploadMessageImages: RequestHandler = async (
         }
 
         const date = Date.now();
-        const filename = "uploads/userImages/" + file.originalname;
-        //renameSync(file.path, filename);
+        const filename = "uploads/userImages/" + date + file.originalname;
+        renameSync(file.path, filename);
         //filenames.push(filename);
 
         const fileContent = fs.readFileSync(filename);
